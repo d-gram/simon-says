71ff86230a503ea65990b5e6f7d148657b36629a
David Gramskih <154123659+d-gram@users.noreply.github.com>
2024-04-29 22:07:02 +1000

    Update buzzer.h

--- include/buzzer.h
+++ include/buzzer.h
@@ -1,16 +1,12 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
- /***
-  * Ex 12.2
-  * Determine your frequencies according to your student number, and 
-  * work out appropriate values for TCA0.SINGLE.PER in each case.  
-  * Update the #defines below to reflect these values.
+ /**
+  * Determine your frequencies and update the #defines below to reflect these values.
   */
 #define TONE1_PER 9662
 #define TONE2_PER 11494
 #define TONE3_PER 7231
 #define TONE4_PER 19268
-// Write your code for Ex 12.2 above this line
 
 void buzzer_init(void);
